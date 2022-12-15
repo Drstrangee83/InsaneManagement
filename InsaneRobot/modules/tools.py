610a6a15fccd77a3fdd2c553cc7d5fd0a0e3866e
Devil3420 <115443754+TheTeamInsane@users.noreply.github.com>
2022-12-15 22:34:19 +0530

    insane

---
+++ InsaneRobot/modules/tools.py
@@ -0,0 +1,10 @@
+# © @Kattai_Massom
+
+__mod_name__ = "Tᴏᴏʟs"
+
+__help__ = """
+
+*Converts*
+ ❍ /encrypt*:* Encrypts The Given Text
+ ❍ /decrypt*:* Decrypts Previously Ecrypted Text
+"""
---
+++ Procfile
@@ -0,0 +1,2 @@
+worker: python3 -m InsaneRobot
+ps:scale worker=1
